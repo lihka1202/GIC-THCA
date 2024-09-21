abeb719beba47184da2a054bb87f6948df3a9f48
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-09-21 20:33:15 +0800

    chore: update attribute to contain bound

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,6 +4,9 @@ public class Car {
     private int yCoordinate;
     private char direction;
 
+    private int xGridBounds;
+    private int yGridBounds;
+
     public Car(String carName, int xCoordinate, int yCoordinate, char direction) {
         this.carName = carName;
         this.xCoordinate = xCoordinate;
@@ -27,13 +30,13 @@ public class Car {
         return direction;
     }
 
-    public void moveCar() {
+    public void moveCarForward() {
         // You need to move the car, ie change the position somehow
         this.xCoordinate += 1;
         this.yCoordinate += 1;
     }
 
-    public void rotateCar() {
+    public void rotateCar(char command) {
         this.direction = 'S';
     }
 }
