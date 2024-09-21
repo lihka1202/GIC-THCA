80a90c8be27ca4cf4cb3463f4ab2a5cc335f9847
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-09-21 20:09:41 +0800

    chore: add methods to test for Car

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,39 @@
 public class Car {
+    private String carName;
+    private int xCoordinate;
+    private int yCoordinate;
+    private char direction;
+
+    public Car(String carName, int xCoordinate, int yCoordinate, char direction) {
+        this.carName = carName;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.direction = direction;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void moveCar() {
+        // You need to move the car, ie change the position somehow
+        this.xCoordinate += 1;
+        this.yCoordinate += 1;
+    }
+
+    public void rotateCar() {
+        this.direction = 'S';
+    }
 }
