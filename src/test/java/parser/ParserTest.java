b6b41a78f1b40a3192988c3714e74e9a3a2bc676
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-09-22 10:58:59 +0800

    chore: failing tests complete for Parser

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -9,13 +9,86 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
     @Test
-    void shouldAggregateSomeCarsFromFile() {
-        Parser parser = new Parser("random.txt");
+    void shouldCollectOneCarFromFile() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
         try {
             Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
         } catch (IOException e) {
             fail("File cannot be opened");
         }
+    }
 
+    @Test
+    void shouldCollectMultipleCarsFromFile() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        try {
+            Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+
+    @Test
+    void shouldAvoidOneCarOutsideLeftmostBound() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        try {
+            Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+    @Test
+    void shouldAvoidOneCarOutsideRightmostBound() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        try {
+            Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+    @Test
+    void shouldAvoidOneCarOutsideUppermostBound() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        try {
+            Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+    @Test
+    void shouldAvoidOneCarOutsideLowermostBound() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        try {
+            Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+
+    @Test
+    void shouldAvoidCarWithNegativeXCoordinate() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        try {
+            Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+    @Test
+    void shouldAvoidCarWithNegativeYCoordinate() {
+        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        try {
+            Car[] listOfCars = parser.parseFile();
+            assertEquals(1, listOfCars.length);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
     }
 }
