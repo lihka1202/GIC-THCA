121b5b8a813a9c2e799929e09dc2e1b28a9d4d01
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-09-23 00:16:54 +0800

    chore: 60% test coverage done

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -12,30 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParserTest {
     @Test
     void shouldCollectOneCarFromFile() {
-        Parser parser = new Parser("src/test/java/parser/test-one.txt");
-        try {
-            Car[] listOfCars = parser.parseFile();
-            assertEquals(1, listOfCars.length);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
-    }
-
-
-    @Test
-    void shouldAvoidOneCarOutsideLeftmostBound() {
-        Parser parser = new Parser("src/test/java/parser/test-one.txt");
-        try {
-            Car[] listOfCars = parser.parseFile();
-            assertEquals(0, listOfCars.length);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
-    }
-
-    @Test
-    void shouldAvoidOneCarOutsideRightmostBound() {
-        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+        Parser parser = new Parser("src/test/java/parser/test_single_car.txt");
         try {
             Car[] listOfCars = parser.parseFile();
             assertEquals(1, listOfCars.length);
@@ -45,53 +22,124 @@ class ParserTest {
     }
 
     @Test
-    void shouldAvoidOneCarOutsideUppermostBound() {
-        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+    void shouldCollectMultipleCarsFromFile() {
+        Parser parser = new Parser("src/test/java/parser/test_multi_car.txt");
         try {
             Car[] listOfCars = parser.parseFile();
+            assertEquals(2, listOfCars.length);
+            assertEquals("A", listOfCars[0].getCarName());
+            assertEquals("B", listOfCars[1].getCarName());
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+
+
+    @Test
+    void shouldAvoidOneCarOutsideRightmostBoundFromSingleCarParse() {
+        Parser parser = new Parser("src/test/java/parser/test_single_car_right_out_of_bounds.txt");
+        try {
+            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outputStreamCaptor));
+            Car[] listOfCars = parser.parseFile();
+
+            // Check if the error message is printed out to console
+            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
+
+
             assertEquals(0, listOfCars.length);
+            System.setOut(originalOut);
         } catch (IOException e) {
             fail("File cannot be opened");
         }
     }
 
     @Test
-    void shouldAvoidOneCarOutsideLowermostBound() {
-        Parser parser = new Parser("src/test/java/parser/test-one.txt");
+    void shouldAvoidOneCarOutsideRightmostBoundFromMultiCarParse() {
+        Parser parser = new Parser("src/test/java/parser/test_multi_car_right_out_of_bounds.txt");
         try {
+            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outputStreamCaptor));
             Car[] listOfCars = parser.parseFile();
+
+            // Check if the error message is printed out to console
+            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
+
+
             assertEquals(0, listOfCars.length);
+            System.setOut(originalOut);
         } catch (IOException e) {
             fail("File cannot be opened");
         }
     }
 
     @Test
-    void shouldAvoidCarWithNegativeXCoordinate() {
-        Parser parser = new Parser("src/test/java/parser/test-negativecoords.txt");
+    void shouldAvoidOneCarOutsideUppermostBoundFromSingleCarParse() {
+        Parser parser = new Parser("src/test/java/parser/test_single_car_upper_out_of_bounds.txt");
         try {
+            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outputStreamCaptor));
             Car[] listOfCars = parser.parseFile();
+
+            // Check if the error message is printed out to console
+            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
+
+
             assertEquals(0, listOfCars.length);
+            System.setOut(originalOut);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+    @Test
+    void shouldAvoidOneCarOutsideUppermostBoundFromMultiCarParse() {
+        Parser parser = new Parser("src/test/java/parser/test_multi_car_upper_out_of_bounds.txt");
+        try {
+            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outputStreamCaptor));
+            Car[] listOfCars = parser.parseFile();
+
+            // Check if the error message is printed out to console
+            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
+
+
+            assertEquals(0, listOfCars.length);
+            System.setOut(originalOut);
         } catch (IOException e) {
             fail("File cannot be opened");
         }
     }
 
+
     @Test
-    void shouldAvoidCarWithNegativeYCoordinate() {
-        Parser parser = new Parser("src/test/java/parser/test-negativecoords.txt");
+    void shouldAvoidCarWithNegativeCarCoordinates() {
+        Parser parser = new Parser("src/test/java/parser/test_negative_coordinates.txt");
         try {
+            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+            PrintStream originalOut = System.out;
+            System.setOut(new PrintStream(outputStreamCaptor));
             Car[] listOfCars = parser.parseFile();
+
+            // Check if the error message is printed out to console
+            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are negative, this is not accepted"));
+
+
             assertEquals(0, listOfCars.length);
+            System.setOut(originalOut);
         } catch (IOException e) {
             fail("File cannot be opened");
         }
     }
+
 
     @Test
     void avoidParsingEmptyFile() {
         try {
-            Parser parser = new Parser("src/test/java/parser/test-empty.txt");
+            Parser parser = new Parser("src/test/java/parser/test_empty.txt");
 
             // Check the output of the code
             ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
@@ -113,7 +161,7 @@ class ParserTest {
     @Test
     void avoidParsingFileWithNegativeGridHeight() {
         try {
-            Parser parser = new Parser("src/test/java/parser/test-negativegrid.txt");
+            Parser parser = new Parser("src/test/java/parser/test_negative_grid_coordinates.txt");
 
             // Check the output of the code
             ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
@@ -135,7 +183,7 @@ class ParserTest {
     @Test
     void avoidParsingFileWithNegativeGridWidth() {
         try {
-            Parser parser = new Parser("src/test/java/parser/test-negativegrid.txt");
+            Parser parser = new Parser("src/test/java/parser/test_negative_grid_coordinates.txt");
 
             // Check the output of the code
             ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
@@ -154,15 +202,6 @@ class ParserTest {
         }
     }
 
-    @Test
-    void shouldCollectMultipleCarsFromFile() {
-        Parser parser = new Parser("src/test/java/parser/test-one.txt");
-        try {
-            Car[] listOfCars = parser.parseFile();
-            assertEquals(1, listOfCars.length);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
-    }
+
 
 }
