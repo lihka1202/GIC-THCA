75b1fd9f6e17966e405d3e3379ab99436251b831
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-09-22 18:11:31 +0800

    chore: basic parser structure written, needs perf improvements

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -19,16 +19,37 @@ public class Parser {
             int xCoord = Integer.parseInt(firstLineInfo[0]);
             int yCoord = Integer.parseInt(firstLineInfo[1]);
             char direction = firstLineInfo[2].charAt(0);
+
+            // Check if the coordinates are negative or if the direction is outside NESW
+            if (xCoord < 0 || yCoord < 0) {
+                System.out.println("Coordinates of the car are negative, this is not accepted");
+                return new Car[]{};
+            } else if ("NESW".indexOf(direction) == -1) {
+                // Direction is something else, not NESW
+                System.out.println("Direction is outside NESW, this is not accepted");
+                return new Car[]{};
+            }
+
             char[] commandLine = reader.readLine().toCharArray();
-            return new Car[]{new Car("SingelCar", xCoord, yCoord, direction, xGridBounds, yGridBounds, commandLine)};
+
+            // Check if the list of commands are valid
+            for (char command : commandLine) {
+                if (command != 'F' && command != 'R' && command != 'L') {
+                    System.out.println("Movement commands contain an unknown character, not accepted");
+                    return new Car[]{};
+                }
+            }
+
+            // All the information is right, return a new car array
+            return new Car[]{new Car("SingleCar", xCoord, yCoord, direction, xGridBounds, yGridBounds, commandLine)};
         } catch (IOException e) {
-            System.out.println("File structure is wrong");
+            System.out.println("File structure is wrong, some lines are missing");
             return new Car[]{};
         }
 
     }
 
-    private Car[] parseMultiCar(BufferedReader reader, String positionLine) {
+    private Car[] parseMultiCar(BufferedReader reader, String positionLine, int xGridBounds, int yGridBounds) {
         return new Car[]{};
     }
 
@@ -50,7 +71,13 @@ public class Parser {
             return new Car[]{};
         }
 
-        return new Car[]{};
+        line = reader.readLine();
+        if (Character.isDigit(line.charAt(0))) {
+            // If the first number is a digit
+            return this.parseSingleCar(reader, line, gridWidth, gridHeight);
+        } else {
+            return this.parseMultiCar(reader, line, gridWidth, gridHeight);
+        }
 
     }
 }
