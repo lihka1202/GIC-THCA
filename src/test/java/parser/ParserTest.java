572cf12b84414431ae105c3e32ee2e0a96e65fde
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-09-23 20:51:59 +0800

    chore: refactor parser tests

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -10,338 +10,144 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
-    @Test
-    void shouldCollectOneCarFromFile() {
-        Parser parser = new Parser("src/test/java/parser/test_single_car.txt");
+
+
+    private String captureOutputDuringParsing(Parser parser) throws IOException {
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;  // Save the original System.out
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        try {
+            parser.parseFile();
+        } finally {
+            System.setOut(originalOut);  // Always restore System.out, even if there's an exception
+        }
+
+        return outputStreamCaptor.toString().trim();  // Return the captured output
+    }
+
+    // Helper method for parsing and checking car count
+    private void assertCarCountFromFile(String filePath, int expectedCount) {
+        Parser parser = new Parser(filePath);
         try {
             Car[] listOfCars = parser.parseFile();
-            assertEquals(1, listOfCars.length);
-            assertEquals('N', listOfCars[0].getDirection());
-            assertEquals(1, listOfCars[0].getxCoordinate());
-            assertEquals(2, listOfCars[0].getyCoordinate());
+            assertEquals(expectedCount, listOfCars.length);
         } catch (IOException e) {
             fail("File cannot be opened");
         }
+    }
+
+    // Helper method for parsing and checking console output and car list size
+    private void assertCarParseWithErrorMessage(String filePath, String expectedMessage) {
+        Parser parser = new Parser(filePath);
+        try {
+            String output = captureOutputDuringParsing(parser);
+
+            // Check if the error message is printed out to console
+            assertTrue(output.contains(expectedMessage));
+
+            // Check if no cars were parsed
+            assertCarCountFromFile(filePath, 0);
+        } catch (IOException e) {
+            fail("File cannot be opened");
+        }
+    }
+
+    @Test
+    void shouldCollectOneCarFromFile() {
+        assertCarCountFromFile("src/test/java/parser/test_single_car.txt", 1);
     }
 
     @Test
     void shouldCollectMultipleCarsFromFile() {
-        Parser parser = new Parser("src/test/java/parser/test_multi_car.txt");
-        try {
-            Car[] listOfCars = parser.parseFile();
-            assertEquals(2, listOfCars.length);
-            assertEquals("A", listOfCars[0].getCarName());
-            assertEquals("B", listOfCars[1].getCarName());
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarCountFromFile("src/test/java/parser/test_multi_car.txt", 2);
     }
-
 
     @Test
     void shouldAvoidOneCarOutsideRightmostBoundFromSingleCarParse() {
-        Parser parser = new Parser("src/test/java/parser/test_single_car_right_out_of_bounds.txt");
-        try {
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
-
-
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_single_car_right_out_of_bounds.txt",
+                "Coordinates of the car are outside bounds, this is not accepted");
     }
 
     @Test
     void shouldAvoidOneCarOutsideRightmostBoundFromMultiCarParse() {
-        Parser parser = new Parser("src/test/java/parser/test_multi_car_right_out_of_bounds.txt");
-        try {
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
-
-
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_multi_car_right_out_of_bounds.txt",
+                "Coordinates of the car are outside bounds, this is not accepted");
     }
 
     @Test
     void shouldAvoidOneCarOutsideUppermostBoundFromSingleCarParse() {
-        Parser parser = new Parser("src/test/java/parser/test_single_car_upper_out_of_bounds.txt");
-        try {
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
-
-
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_single_car_upper_out_of_bounds.txt",
+                "Coordinates of the car are outside bounds, this is not accepted");
     }
 
     @Test
     void shouldAvoidOneCarOutsideUppermostBoundFromMultiCarParse() {
-        Parser parser = new Parser("src/test/java/parser/test_multi_car_upper_out_of_bounds.txt");
-        try {
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are outside bounds, this is not accepted"));
-
-
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_multi_car_upper_out_of_bounds.txt",
+                "Coordinates of the car are outside bounds, this is not accepted");
     }
-
 
     @Test
     void shouldAvoidCarWithNegativeCarCoordinates() {
-        Parser parser = new Parser("src/test/java/parser/test_negative_coordinates.txt");
-        try {
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Coordinates of the car are negative, this is not accepted"));
-
-
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_negative_coordinates.txt",
+                "Coordinates of the car are negative, this is not accepted");
     }
-
 
     @Test
     void avoidParsingEmptyFile() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_empty.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("The file is empty, please ensure that it contains the relevant fields"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_empty.txt",
+                "The file is empty, please ensure that it contains the relevant fields");
     }
 
     @Test
     void avoidParsingFileWithNegativeGridHeight() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_negative_grid_coordinates.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("GridWidth or GridHeight is less than 0, please modify this and try again"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_negative_grid_coordinates.txt",
+                "GridWidth or GridHeight is less than 0, please modify this and try again");
     }
 
     @Test
     void avoidParsingFileWithNegativeGridWidth() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_negative_grid_coordinates.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("GridWidth or GridHeight is less than 0, please modify this and try again"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_negative_grid_coordinates.txt",
+                "GridWidth or GridHeight is less than 0, please modify this and try again");
     }
-
-    //TODO: Handle case with malforammted files
 
     @Test
     void shouldAvoidSingleCarWithWrongCommands() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_single_car_wrong_commands.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Movement commands contain an unknown character, not accepted"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_single_car_wrong_commands.txt",
+                "Movement commands contain an unknown character, not accepted");
     }
 
     @Test
     void shouldAvoidMultiCarWithWrongCommands() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_multi_car_wrong_commands.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Movement commands contain an unknown character, not accepted"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_multi_car_wrong_commands.txt",
+                "Movement commands contain an unknown character, not accepted");
     }
 
     @Test
     void shouldAvoidMultiCarWithWrongDirection() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_multi_car_wrong_direction.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Direction is outside NESW, this is not accepted"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_multi_car_wrong_direction.txt",
+                "Direction is outside NESW, this is not accepted");
     }
 
     @Test
     void shouldAvoidSingleCarWithWrongDirection() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_single_car_wrong_direction.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("Direction is outside NESW, this is not accepted"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_single_car_wrong_direction.txt",
+                "Direction is outside NESW, this is not accepted");
     }
 
     @Test
     void shouldAvoidSingleCarWithWrongFileStructure() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_single_car_wrong_file_structure.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("File structure is wrong, this is not accepted"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_single_car_wrong_file_structure.txt",
+                "File structure is wrong, this is not accepted");
     }
 
     @Test
     void shouldAvoidMultiCarWithWrongFileStructure() {
-        try {
-            Parser parser = new Parser("src/test/java/parser/test_multi_car_wrong_file_structure.txt");
-
-            // Check the output of the code
-            ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-            PrintStream originalOut = System.out;
-            System.setOut(new PrintStream(outputStreamCaptor));
-            Car[] listOfCars = parser.parseFile();
-
-            // Check if the error message is printed out to console
-            assertTrue(outputStreamCaptor.toString().contains("File structure is wrong, this is not accepted"));
-
-            // Check if the returned number of cars is 0, as expected
-            assertEquals(0, listOfCars.length);
-            System.setOut(originalOut);
-        } catch (IOException e) {
-            fail("File cannot be opened");
-        }
+        assertCarParseWithErrorMessage("src/test/java/parser/test_multi_car_wrong_file_structure.txt",
+                "File structure is wrong, this is not accepted");
     }
 
-
-    // Integration Test for this level
+    // Integration Test for checking car's final position
     @Test
     void shouldReachRelevantPosition() {
         Parser parser = new Parser("src/test/java/parser/test_single_car.txt");
@@ -353,10 +159,8 @@ class ParserTest {
             assertEquals(4, listOfCars[0].getxCoordinate());
             assertEquals(3, listOfCars[0].getyCoordinate());
             assertEquals('S', listOfCars[0].getDirection());
-
         } catch (IOException e) {
             fail("File cannot be opened");
         }
     }
-
 }
